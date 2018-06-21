463adad2e684686973dcd58c29a98e14034c0a95
Woraphot Chokratanasombat <wc@asiola.com>
2018-06-21 12:29:59 +0700

    [Base Activity] Change: Clean up source code

--- app/src/main/java/com/example/android/pets/BaseActivity.kt
+++ app/src/main/java/com/example/android/pets/BaseActivity.kt
@@ -1,13 +1,9 @@
 package com.example.android.pets
 
 import android.annotation.SuppressLint
-import android.content.ContentUris
-import android.content.ContentValues
-import android.database.Cursor
 import android.support.v4.app.NavUtils
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
-import com.example.android.pets.data.PetContract
 
 @SuppressLint("Registered")
 abstract class BaseActivity : AppCompatActivity() {
